b82608526f18ebd2aa4d02d1c6498435cc723b7d
faresCherif <fares.cherif@etu.unilim.fr>
2022-02-21 00:04:43 +0100

    3e activite fin

--- app/src/main/java/com/example/projetihm/UserInput.java
+++ app/src/main/java/com/example/projetihm/UserInput.java
@@ -2,13 +2,38 @@ package com.example.projetihm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class UserInput extends AppCompatActivity {
+    private EditText firstName;
+    private EditText lastName;
+    public static final String TAG = "UserInpur";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_input);
+    }
+
+    public void toast(String msg) {
+        Toast.makeText(this, msg,Toast.LENGTH_SHORT).show();
+    }
+
+    public void callQuestion1(android.view.View v){
+        firstName=findViewById(R.id.FirstNameEntreeArea);
+        lastName=findViewById(R.id.NameEntreeArea);
+
+
+        if(!firstName.getText().toString().isEmpty()&&!lastName.getText().toString().isEmpty()) {
+            Intent i = new Intent(UserInput.this, Question1.class);
+            startActivity(i);
+        }
+        else{
+            toast("Veuillez entrer un nom et prenom");
+        }
     }
 }
