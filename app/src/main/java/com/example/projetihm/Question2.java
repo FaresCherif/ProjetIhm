54aafd59987bde189c606c8640b32a1e11dcaefe
faresCherif <fares.cherif@etu.unilim.fr>
2022-02-21 11:59:53 +0100

    question 3 end

--- app/src/main/java/com/example/projetihm/Question2.java
+++ app/src/main/java/com/example/projetihm/Question2.java
@@ -2,6 +2,7 @@ package com.example.projetihm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Question2 extends AppCompatActivity {
@@ -11,4 +12,10 @@ public class Question2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question2);
     }
+
+    public void callNextQuestion(android.view.View v) {
+        Intent i =new Intent(Question2.this, Question3.class);
+        startActivity(i);
+    }
+
 }
