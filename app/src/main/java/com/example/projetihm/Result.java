1d429bd8c3199e41d703cc831a61ab97ce4abcab
faresCherif <fares.cherif@etu.unilim.fr>
2022-02-23 01:31:53 +0100

    result multiple

--- app/src/main/java/com/example/projetihm/Result.java
+++ app/src/main/java/com/example/projetihm/Result.java
@@ -3,12 +3,16 @@ package com.example.projetihm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Result extends AppCompatActivity {
     private User u;
     private TextView fullName;
+    private ImageView image;
+    private TextView texteResult;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +21,36 @@ public class Result extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
         fullName = findViewById(R.id.FullName);
+        image= findViewById(R.id.ImageResult);
+        texteResult=findViewById(R.id.ResultText);
 
         fullName.setText(u.getFirstname()+" "+u.getName());
         toast(u.getRomanceLevel()+" "+u.getTemerityLevel());
+
+
+        if(u.getTemerityLevel()>5 && u.getRomanceLevel() > 5){
+            image.setImageResource(R.drawable.resultlove);
+            texteResult.setText(R.string.perfectLover);
+        }
+        if(between(u.getTemerityLevel(),0,5) && between(u.getRomanceLevel(),0,5)){
+            image.setImageResource(R.drawable.resultlove);
+            texteResult.setText(R.string.balancedLover);
+        }
+        if(u.getTemerityLevel()>10 && u.getRomanceLevel() < 5){
+            image.setImageResource(R.drawable.resultlove);
+            texteResult.setText(R.string.temerityLover);
+        }
+        if(u.getTemerityLevel()<5 && u.getRomanceLevel() > 10){
+            image.setImageResource(R.drawable.resultlove);
+            texteResult.setText(R.string.romanceLover);
+        }
+    }
+
+    public boolean between(float valLooked,float min,float max){
+        if(valLooked>=min && valLooked<=max){
+            return true;
+        }
+        return false;
     }
 
     public void toast(String msg) {
