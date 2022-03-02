84f63ae2a68eb7579d71336490d50b69c349b48c
faresCherif <fares.cherif@etu.unilim.fr>
2022-03-02 15:20:29 +0100

    dice working

--- app/src/main/java/com/example/projetihm/MainActivity.java
+++ app/src/main/java/com/example/projetihm/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //verifyStoragePermissions(this);
+        verifyStoragePermissions(this);
     }
 
     public void load(android.view.View v){
